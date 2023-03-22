d2089864b31cae098f89e7aa00636621a780ace5
James <jamesglendenning1@gmail.com>
2023-03-22 00:08:04 +0000

    Added remove branding class

---
+++ controllers/services/class-branding.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Branding
+ *
+ * @package totally-not-wordpress
+ * @version 1.0.0
+ */
+
+namespace TNWP\TNWP_Branding;
+
+use TNWP\Totally_Not_WordPress;
+
+/**
+ * Remove WordPress Branding.
+ */
+class TNWP_Branding extends Totally_Not_WordPress {
+
+    // Construct.
+    public function __construct() {
+        $this->filters();
+        $this->actions();
+	}
+
+    public function actions() : void {
+        add_action( 'wp_before_admin_bar_render', array( $this, 'remove_admin_bar_logo' ), 10 );
+    }
+
+    public function filters() : void {
+        add_filter( 'admin_footer_text', array( $this, 'remove_admin_footer_text' ), 600 );
+        add_filter( 'update_footer', array( $this, 'remove_admin_version' ), 600 );
+    }
+
+    /**
+     * Removes admin bar logo.
+     * 
+     * @return void
+     */
+    public function remove_admin_bar_logo() : void {
+        global $wp_admin_bar;
+
+        $wp_admin_bar->remove_menu( 'wp-logo' );
+    }
+
+    /**
+     * Removes admin footer text.
+     * 
+     * @param string $text
+     * 
+     * @param string Return an empty string.
+     */
+    public function remove_admin_footer_text( string $text ) : string {
+        return '';
+    }
+
+    /**
+     * Remove admin version number from footer.
+     * 
+     * @param string $version
+     * 
+     * @return string Empty version string.
+     */
+    public function remove_admin_version( string $version ) : string {
+        return '';
+    }
+}
+
+new TNWP_Branding();
