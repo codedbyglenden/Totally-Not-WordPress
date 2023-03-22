b676238f7f85c47e8999ff2d59ae52e3b9e5c27d
James <jamesglendenning1@gmail.com>
2023-03-22 09:45:15 +0000

    Remove custom css

--- controllers/services/class-branding.php
+++ controllers/services/class-branding.php
@@ -23,11 +23,23 @@ class TNWP_Branding extends Totally_Not_WordPress {
 
     public function actions() : void {
         add_action( 'wp_before_admin_bar_render', array( $this, 'remove_admin_bar_logo' ), 10 );
+        add_action( 'customize_register', array( $this, 'remove_css_section' ), 15 );
+
+        // .
     }
 
     public function filters() : void {
         add_filter( 'admin_footer_text', array( $this, 'remove_admin_footer_text' ), 600 );
         add_filter( 'update_footer', array( $this, 'remove_admin_version' ), 600 );
+    }
+
+    /**
+     * Remove the additional CSS section, introduced in 4.7, from the Customizer.
+     *
+     * @return void
+     */
+    public function remove_css_section( \WP_Customize_Manager $wp_customize ) : void {
+        $wp_customize->remove_section( 'custom_css' );
     }
 
     /**
---
+++ controllers/services/class-settings.php
@@ -0,0 +1,2 @@
+<!-- Add a settings page that allows us to alter settings like the login url -->
+
