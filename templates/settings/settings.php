0648fb4a74c2f93f968bfbce2506f2b3f83a0e2f
James <jamesglendenning1@gmail.com>
2023-04-02 13:48:40 +0100

    Update settings.php

--- templates/settings/settings.php
+++ templates/settings/settings.php
@@ -10,3 +10,10 @@
  */
 $plugin->get_template_part('settings/partials/header.php');
 
+// We should probably write a plugin form generator for settings pages.
+
+// The method invokes a class which contains render methods & save functionality.
+
+// The invoked method should be a child class of a form class.
+
+// This way we can share helpers to process data & output form fields.
