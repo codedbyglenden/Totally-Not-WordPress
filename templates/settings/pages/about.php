ec83f49065a1ce95ef0e4c0b28a909a9ca02a91b
James <jamesglendenning1@gmail.com>
2023-04-03 17:00:52 +0100

    Added settings page metaboxes

--- templates/settings/pages/about.php
+++ templates/settings/pages/about.php
@@ -1,1 +1,31 @@
-The about page
+<section class="grid">
+    <article class="tnwp-metabox is-half">
+        <div class="content">
+            <h2>Tell me about this framework</h2>
+            <p>
+                Inspired by <a target="_blank" href="https://github.com/devinvinson/WordPress-Plugin-Boilerplate/">WordPress Plugin Boilerplate</a> by
+                Devin Vinson, Totally not wordpress is a plugin boilerplate for the modern web. 
+            </p>
+        </div>
+    </article>
+
+    <article class="tnwp-metabox is-half">
+        <div class="content">
+            <h2>You can find my other work</h2>
+            <ul>
+                <li>
+                    <a target="_blank" href="https://github.com/codedbyglenden">Github</a>
+                </li>
+                <li>
+                    <a target="_blank" href="https://www.instagram.com/coded.by.glenden">Instagram</a>
+                </li>
+                <li>
+                    <a target="_blank" href="https://www.linkedin.com/in/codedbyglenden">LinkedIn</a>
+                </li>
+                <li>
+                    <a target="_blank" href="https://codedbyglenden.com">My Portfolio</a>
+                </li>
+            </ul>
+        </div>
+    </article>
+</div>
